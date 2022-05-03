0f429b0a0e2454de0a60520307eb46ad23396ff0
yuz120 <yuz120@ucsd.edu>
2022-05-02 22:29:03 -0700

    Update copyList.java

--- copyList.java
+++ copyList.java
@@ -1,1 +1,22 @@
+public class copyList<E> {
+    public Object[] intArr; 
+    int size;
+    int DEFAULT_CAPACITY=10;
 
+    public copyList (E[] arr1){
+        if(arr1==null){
+            this.intArr=new Object[DEFAULT_CAPACITY];          
+        }
+        if(arr1!=null){
+            this.intArr=new Object [arr1.length];
+            this.intArr=arr1;
+            size=arr1.length;
+        }
+        
+    }
+
+    public int returnSize(){
+        return this.size;
+
+    }
+}
