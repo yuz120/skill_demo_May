cd540d303a111135bbe83a5de793df32658303c0
yuz120 <yuz120@ucsd.edu>
2022-05-02 22:31:24 -0700

    add failed test

---
+++ copyListTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class copyListTest {
+    Integer[] arr = {1,2,3,4,5};
+    copyList listTest;
+    private void setUp(){
+        listTest = new copyList(arr);
+    }
+
+
+    
+    @Test
+    public void sizeTester(){
+        setUp();
+        assertEquals("expected", 4,listTest.returnSize()); //failed test
+
+    
+}
